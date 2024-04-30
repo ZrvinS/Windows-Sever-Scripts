5fb0c8946cc9a1e79d818ed15bdf85d14b7bff60
ZrvinS <zrvinsunar@gmail.com>
2024-04-30 18:00:17 +0000

    updateforall

--- HBAUpdated.ps1
+++ HBAUpdated.ps1
@@ -4,10 +4,10 @@ $reachableServers = Get-Content("./servers.txt") | Test-NetConnection | ? { $_.P
 $result = @()
 $result = foreach($server in $reachableServers.ComputerName){   
             
-    Get-WmiObject win32_PnpSignedDriver -ComputerName $server | ?{$_.DeviceName -match "HPE S*"} | Select-Object __SERVER, Description, DriverVersion
-    Get-WmiObject win32_PnpSignedDriver -ComputerName $server | ?{$_.DeviceName -match "QLOGIC*"} | Select-Object __SERVER, Description, DriverVersion 
-
-
+    Get-WmiObject win32_PnpSignedDriver -ComputerName $server | ?{$_.DeviceName -match "HPE S*"} | Select-Object __SERVER,DriverProviderName, Description, DriverVersion
+    Get-WmiObject win32_PnpSignedDriver -ComputerName $server | ?{$_.DeviceName -match "QLOGIC*"} | Select-Object __SERVER,DriverProviderName, Description, DriverVersion 
+    Get-WmiObject win32_PnpSignedDriver -ComputerName $server | ?{$_.DeviceName -match "HP FlexFabric*"} | Select-Object __SERVER, DriverProviderName,Description, DriverVersion -First 1
+    Get-WmiObject win32_PnpSignedDriver -ComputerName $server | ?{$_.DeviceName -match "Emulex*"} | Select-Object __SERVER, DriverProviderName,FriendlyName, Description, DriverVersion -First 1
 }
 
 $result| Out-File -FilePath ".\HBAResult.txt" -Force
