5bd3d3f73a9eed46b9971d0b2ee264a42e93cee3
ZrvinS <zrvinsunar@gmail.com>
2025-07-18 00:51:27 +0530

    Update

---
+++ FirmwareVersionFromILO.ps1
@@ -0,0 +1,17 @@
+cd C:\Users\it0777su\Documents
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\hpeilocmdlets.4.4.0\HPEiLOCmdlets.psd1"
+$ilolist = Get-Content -Path ".\ilolist.txt"
+$ilouser = "Admin"
+$ilopassword = ""
+$result = @()
+$result += foreach ($ilo in $ilolist){
+    $iloconnection = Connect-HPEiLO -Address $ilo -Username $ilouser -Password $ilopassword -DisableCertificateAuthentication
+    $firmware = Get-HPEiLOFirmwareInventory -Connection $iloconnection
+    $firmware.FirmwareInformation    
+    Write-Output $ilo $serverDrive $Health $State
+    Write-Output "--------------------------------------------------------------------------------------------------------------------------------"
+
+}
+
+$result | Out-File -FilePath "./iloriskresult2.txt" -force
+
